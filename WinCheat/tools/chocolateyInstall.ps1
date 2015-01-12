fdfe9eba4851811c50576c3b372002a160c0df1e
iggi131 <iggi131@slovanet.sk>
2015-01-12 19:12:30 +0100

    added new packages

---
+++ WinCheat/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿if(-not (test-path "hklm:\SOFTWARE\Classes\Installer\Products\068CE4D61134FE84E9455686288FAD77"))
+{
+    Install-ChocolateyPackage 'wincheat' 'exe' '/S /v/qn' 'http://alinconstantin.dtdns.net/Download/WinCheat/Versions/1.7/WinCheatSetup.exe'
+}
+else
+{
+    Write-Host "WinCheat is already installed on your machine."
+} 
