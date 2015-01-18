325aff24dd7c9198d8b67df80c1ba16ab428ef38
iggi131 <iggi131@slovanet.sk>
2015-01-18 22:48:09 +0100

    proxytrace

---
+++ ProxyTrace/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$binRoot = Get-BinRoot
+Write-Debug "Bin Root is $binRoot"
+$installDir = Join-Path "$binRoot" "$packageName"
+
+Install-ChocolateyZipPackage 'proxytrace' 'http://www.pocketsoap.com/tcptrace/proxyTrace012.zip' $installDir
+Install-ChocolateyPath $installDir 'User'
