e6a503528fb5ca0e572f785c09c3f9c6fb0b9af5
iggi131 <iggi131@slovanet.sk>
2015-01-18 22:44:48 +0100

    tcptrace

---
+++ TcpTrace/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$binRoot = Get-BinRoot
+Write-Debug "Bin Root is $binRoot"
+$installDir = Join-Path "$binRoot" "$packageName"
+
+Install-ChocolateyZipPackage 'tcptrace' 'http://www.pocketsoap.com/tcptrace/tcpTrace081.zip' $installDir
+Install-ChocolateyPath $installDir 'User'
