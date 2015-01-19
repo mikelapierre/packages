3935816a99da563584ad09a4683ecef617cde7e0
iggi131 <iggi131@slovanet.sk>
2015-01-19 14:21:16 +0100

    dvddecrypter

--- DVD Decrypter/package/tools/chocolateyInstall.ps1
+++ DVD Decrypter/package/tools/chocolateyInstall.ps1
@@ -1,8 +1,1 @@
-﻿if(-not (test-path "hkcu:\Software\DVD Decrypter"))
-{
-    Install-ChocolateyPackage 'dvddecrypter' 'exe' '/S' 'http://www.dvddecrypter.org.uk/SetupDVDDecrypter_3.5.4.0.exe'
-}
-else
-{
-    Write-Host "DVD Decrypter is already installed on your machine."
-} 
+﻿Install-ChocolateyPackage 'dvddecrypter' 'exe' '/S' 'http://www.dvddecrypter.org.uk/SetupDVDDecrypter_3.5.4.0.exe'
