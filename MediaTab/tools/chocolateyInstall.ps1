17093e594607313e77f8f2d5ab6c3654ae0ed968
iggi131 <iggi131@slovanet.sk>
2015-01-19 13:55:40 +0100

    mediatab

--- MediaTab/tools/chocolateyInstall.ps1
+++ MediaTab/tools/chocolateyInstall.ps1
@@ -1,9 +1,2 @@
-﻿if(-not (test-path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MediaTab"))
-{
-    Install-ChocolateyZipPackage 'mediatab' 'http://mediatab.mediaarea.net/MediaTab%20v1.4%20Setup.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-    Install-ChocolateyInstallPackage 'mediatab' 'exe' '/S' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)/MediaTab v1.4 Setup.exe"
-}
-else
-{
-    Write-Host "MediaTab is already installed on your machine."
-} 
+﻿Install-ChocolateyZipPackage 'mediatab' 'http://mediatab.mediaarea.net/MediaTab%20v1.4%20Setup.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyInstallPackage 'mediatab' 'exe' '/S' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)/MediaTab v1.4 Setup.exe"
