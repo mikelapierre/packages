211560fa53bab991cff5842efb0bb378793b4034
iggi131 <iggi131@slovanet.sk>
2015-01-24 22:49:53 +0100

    rawcap

---
+++ RawCap/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$binRoot = Get-BinRoot
+Write-Debug "Bin Root is $binRoot"
+$installDir = Join-Path "$binRoot" "$packageName"
+$rawcapPath = Join-Path $installDir 'RawCap.exe'
+
+if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
+
+Get-ChocolateyWebFile 'rawcap' "$rawcapPath" 'http://www.netresec.com/?download=RawCap&ref=chocolatey'
+Install-ChocolateyPath $installDir 'User'
