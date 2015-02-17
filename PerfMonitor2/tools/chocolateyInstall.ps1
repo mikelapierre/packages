77f87c573657373358ef14eb17c9e46cd1749454
iggi131 <iggi131@slovanet.sk>
2015-02-17 09:42:11 +0100

    perfmonitor2

--- PerfMonitor2/tools/chocolateyInstall.ps1
+++ PerfMonitor2/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'perfmonitor2' 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 'http://www.cpuid.com/medias/files/softwares/perfmonitor/CPUID_PerfMonitor_2.04_setup.exe'
+﻿Install-ChocolateyPackage 'perfmonitor2' 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 'http://www.cpuid.com/downloads/perfmonitor-2/perfmonitor-2_2.04.exe'
