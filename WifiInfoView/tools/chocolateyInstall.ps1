4ee393cad207e1491e7844554969c5009a3175a8
iggi131 <iggi131@slovanet.sk>
2015-05-04 17:20:35 +0200

    WifiInfoView

---
+++ WifiInfoView/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'wifiinfoview'
+$url = 'http://www.nirsoft.net/utils/wifiinfoview.zip'
+$environmentVariableTarget = 'User'
+
+$binRoot = Get-BinRoot
+Write-Debug "Bin Root is $binRoot"
+$installDir = Join-Path "$binRoot" "$packageName"
+
+Install-ChocolateyZipPackage "$packageName" "$url" $installDir
+Install-ChocolateyPath $installDir $environmentVariableTarget
