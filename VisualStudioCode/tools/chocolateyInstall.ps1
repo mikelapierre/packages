3d54e7030487be5f39f2f6e6eca4bad3ab7ce489
iggi131 <iggi131@slovanet.sk>
2015-05-06 18:09:33 +0200

    VisualStudioCode

---
+++ VisualStudioCode/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'visualstudiocode'
+$installerType = 'EXE'
+$url = 'http://download.microsoft.com/download/0/D/5/0D57186C-834B-463A-AECB-BC55A8E466AE/VSCodeSetup.exe'
+$silentArgs = ''
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
