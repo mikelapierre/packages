0d103b119ae463cbe91bd7fe198af9655bda3165
iggi131 <iggi131@slovanet.sk>
2015-05-13 17:36:36 +0200

    WolCmd

---
+++ WolCmd/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'wolcmd'
+$url = 'http://www.depicus.com/downloads/WolCmd.zip'
+$environmentVariableTarget = 'User'
+
+$binRoot = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Write-Debug "Bin Root is $binRoot"
+$installDir = Join-Path "$binRoot" "$packageName"
+
+Install-ChocolateyZipPackage "$packageName" "$url" $installDir
+Install-ChocolateyPath $installDir $environmentVariableTarget
