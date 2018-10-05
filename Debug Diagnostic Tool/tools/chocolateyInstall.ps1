a7645a32d1ab69419e7f4f923d7667d3065816a0
mikelapierre <mike.lapierre@live.com>
2018-10-05 13:15:41 -0400

    Update links

--- Debug Diagnostic Tool/tools/chocolateyInstall.ps1
+++ Debug Diagnostic Tool/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'debugdiagnostic' 'msi' '/quiet' 'http://download.microsoft.com/download/B/4/6/B46E9984-5DF2-4B56-AE32-D60A88C2A6D8/DebugDiagx86.msi' 'http://download.microsoft.com/download/B/4/6/B46E9984-5DF2-4B56-AE32-D60A88C2A6D8/DebugDiagx64.msi'
+﻿Install-ChocolateyPackage 'debugdiagnostic' 'msi' '/quiet' 'https://download.microsoft.com/download/D/C/9/DC98BD0E-5A9A-4D8A-B313-22BC3604FB05/DebugDiagx86.msi' 'https://download.microsoft.com/download/D/C/9/DC98BD0E-5A9A-4D8A-B313-22BC3604FB05/DebugDiagx64.msi'
